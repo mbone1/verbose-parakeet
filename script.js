6d0856ccdf2bca9646a08bb05820560fa308afe8
mbone1 <m.bonnev10@gmail.com>
2020-08-14 06:51:05 -0700

    814 6:50AM -solved targetting issue

--- script.js
+++ script.js
@@ -30,112 +30,188 @@ let slots = [
 function walter(){
   for (let i = 0; i < slots.length; i++) {
     const element = slots[i];
-    let timeBlock = $("<div>")
+    let timeBlock = $("<p>")
     .addClass("col-md-1 hour")
-    .text(slots[i]);
+    .text(slots[i])
+    .val(slots[i]);
     $("#TB").append(timeBlock);
-    var $textArea = $("<textarea>") // ⮘◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ how can i declare this outside of the loop w/ same functionality? ◙ tried
+    var $textArea = $("<textarea>") 
     .addClass("col-md-10 description")
-    .val(["abc"]); // ⮘◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ where input value will be stored
+    .val(""); // ⮘◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ where input value will be stored
     $("#TB").append($textArea);
     let saveButton = $("<button>")
     .addClass("btn saveBtn col-md-1 fas fa-save")
     $("#TB").append(saveButton);
-    // ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ ⮙ RENDERS TIME SLOT, INPUT FIELD AND SAVE BUTTON TO HTML
-    
   }
-  let textAreaVal = $textArea.val(); // ⮘◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ variable to store value of what user has input
-  console.log(textAreaVal)
+// ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ ⮙ RENDERS TIME SLOT, INPUT FIELD AND SAVE BUTTON TO HTML
+// let textAreaVal = $textArea.val(); // ⮘◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ variable to store value of what user has input
+// console.log(textAreaVal)
+
+$(".saveBtn").click(function(){ 
+  //  let dingus = localStorage.getItem("inputField")
+  //  if(dingus){
+  //      dingus = JSON.parse(dingus);
+  //  }else{
+  //    dingus = {};
+  //  }
+  const inputFieldKey = $(this).prev("textarea").prev("p").css("background-color", "red").val();
+  const inputField = $(this).prev("textarea").css("background-color", "red").val();
+  localStorage.setItem(inputFieldKey, inputField);
   
-  $(".saveBtn").click(function(){ 
+  // console.log(this)
+  // console.log(inputField)
+  // console.log(inputFieldKey)
+  // localStorage.Storage.setItem("schedule", JSON.stringify(schedule));
+  // const hourSlot = $(this).prev(".dingus").attr("id");
+  // console.log(hourSlot);
+  
+//   function donny(){
+//   var dingus = localStorage.getItem("inputField")
+//    $("textArea").val = dingus
+//  }
+ 
+//  donny();
+  // console.log(inputField)
+  // console.log(JSON.stringify($textArea.val))
+  // var  = localStorage.getItem("text");
+  // textArea = textArea ? textArea.split(',') : [];
+  // localStorage.setItem("text", textArea.toString());
+  
+}); 
     
-    var textArea = localStorage.getItem("text");
-    textArea = textArea ? textArea.split(',') : [];
-    textArea.push(textAreaVal)
-    localStorage.setItem("text", textArea.toString());
 
-  }); // 
+  
+  
+  // const id = $(this).parent().attr("id");
+  // console.log(id)
+  
+  //jquery access sibling element - may need for my code... read up on jquery parents. simplying
+  // $(".saveBtn").("click", function(){
+    //   let schedule = localStorage.getItem("storage");
+    //   if(schedule){
+      //       schedule = JSON.parse(schedule);
+      //   }else {
+        //     schedule = {};
+        //   }
+        
+        //   for(id in schedule){
+          //     $("#").find("textarea").val(schedule[id]);
+          //   }
+          
+          //   const val = $(this).siblings("textarea")[0])
+          //   console.log(val);
+          //   const id = $(this).parent().attr("id");
+          //   console.log(id);
+          //   schedule[id] = val;
+          //   localStorage.Storage.setItem("schedule", JSON.stringify(scheulde));
+          // })
+          
+        }
+        
+        walter();
+        
+        // ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ ⮙ MAIN FUNCTION TO SAVE TO LOCAL STORAGE 
+        //object for storing user data--------------------------------------------------------------------------------------------------------------------------------------------
+        //object for storing user data--------------------------------------------------------------------------------------------------------------------------------------------
+        //function to change color--------------------------------------------------------------------------------------------------------------------------------------------
+        //function to change color--------------------------------------------------------------------------------------------------------------------------------------------
+        
+        // WHEN I scroll down ⮘◙◙◙◙◙◙ DONE. FORMATTING IS WONKY BUT DOABLE
+        // THEN I am presented with timeblocks for standard business hours ⮘◙◙◙◙◙◙ DONE. FORMATTING IS WONKY BUT DOABLE
+        // WHEN I view the timeblocks for that day
+        // THEN each timeblock is color coded to indicate whether it is in the past, present, or future ⮘◙◙◙◙◙◙ NEED TO WORK ON
+        // WHEN I click into a timeblock 
+        // THEN I can enter an event
+        // WHEN I click the save button fort that timeblock 
+        // THEN the text for that event is saved in local storage ⮘◙◙◙◙◙◙ NEED TO WORK ON
+        // WHEN I refresh the page
+        // THEN the saved events persist ⮘◙◙◙◙◙◙ NEED TO WORK ON
+        
+        // ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ ⮛ ideas ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙
+        // could i use a for loop to loop thru each field before refresh to determine if value contained within should be saved? 
+        // 
+        
+        
+        // ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ ⮛ commented out attempts ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙
+        
+        // for (let i = 0; i < slots.length; i++) {
+          //     const element = slots[i];
+          //     let timeBlock1 = $("<textarea>")
+          //     .addClass("row description col-md-10")
+          //     .attr("data-slot", slots[i])
+          //     $("#TB").append(timeBlock);
+          //     console.log(timeBlock)
+          // }
+          
+          // for (let i = 0; i < slots.length; i++) {
+            //     const element = slots[i];
+            //     let timeBlock2 = $("<button>")
+            //     .addClass("row btn saveBtn col-md-10")
+            //     .attr("data-slot", slots[i])
+            //     $("#TB").append(timeBlock);
+            //     console.log(timeBlock)
+            // }
+            
+            // for (let i = 0; i < slots.length; i++) {
+              //     const element = slots[i];
+              //     let timeBlock = $("<p>")
+              //     .addClass("row description col-md-10")
+              //     .attr("data-slot", slots[i])
+              //     .text(slots[i])
+              //     $("#TB").append(timeBlock);
+              //     console.log(timeBlock)
+              //     for (let i = 8; i < slots.length; i++) {
+                //         const element = slots[i];
+                //         let timeBlock = $("<textarea>")
+                //         .addClass("row description col-md-10")
+                //         .attr("data-slot", slots[i])
+                //         $("#TB").append(timeBlock);
+                //         console.log(timeBlock)
+                //         for (let i = 8; i < slots.length; i++) {
+                  //             const element = slots[i];
+                  //             let timeBlock = $("<button>")
+                  //             .addClass("row btn saveBtn col-md-10")
+                  //             .attr("data-slot", slots[i])
+                  //             $("#TB").append(timeBlock);
+                  //             console.log(timeBlock)
+                  //         }
+                  //     }
+                  // }
+                  
+                  // for (let i = 0; i < letters.length; i++) {
+                    //     const element = letters[i];
+                    //     let letterBtn = $("<button>")
+                    //       .addClass("letter-button letter letter-button-color")
+                    //       .attr("data-letter", letters[i])
+                    //       .text(letters[i]);
+                    //       $("#buttons").append(letterBtn);
+                    
+                    //       console.log(letters);
+                    //     }
+                    
+                    // window.localStorage.getItem(inputField)
+                    // console.log(JSON.parse(window.localStorage.getItem(inputField)));
 
-
-}
-
-walter();
-// ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ ⮙ MAIN FUNCTION TO SAVE TO LOCAL STORAGE 
-//object for storing user data--------------------------------------------------------------------------------------------------------------------------------------------
-//object for storing user data--------------------------------------------------------------------------------------------------------------------------------------------
-//function to change color--------------------------------------------------------------------------------------------------------------------------------------------
-//function to change color--------------------------------------------------------------------------------------------------------------------------------------------
-
-// WHEN I scroll down ⮘◙◙◙◙◙◙ DONE. FORMATTING IS WONKY BUT DOABLE
-// THEN I am presented with timeblocks for standard business hours ⮘◙◙◙◙◙◙ DONE. FORMATTING IS WONKY BUT DOABLE
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future ⮘◙◙◙◙◙◙ NEED TO WORK ON
-// WHEN I click into a timeblock 
-// THEN I can enter an event
-// WHEN I click the save button fort that timeblock 
-// THEN the text for that event is saved in local storage ⮘◙◙◙◙◙◙ NEED TO WORK ON
-// WHEN I refresh the page
-// THEN the saved events persist ⮘◙◙◙◙◙◙ NEED TO WORK ON
-
-// ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ ⮛ ideas ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙
-// could i use a for loop to loop thru each field before refresh to determine if value contained within should be saved? 
-// 
-
-
-// ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ ⮛ commented out attempts ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙
-
-// for (let i = 0; i < slots.length; i++) {
-//     const element = slots[i];
-//     let timeBlock1 = $("<textarea>")
-//     .addClass("row description col-md-10")
-//     .attr("data-slot", slots[i])
-//     $("#TB").append(timeBlock);
-//     console.log(timeBlock)
-// }
-
-// for (let i = 0; i < slots.length; i++) {
-//     const element = slots[i];
-//     let timeBlock2 = $("<button>")
-//     .addClass("row btn saveBtn col-md-10")
-//     .attr("data-slot", slots[i])
-//     $("#TB").append(timeBlock);
-//     console.log(timeBlock)
-// }
-
-// for (let i = 0; i < slots.length; i++) {
-//     const element = slots[i];
-//     let timeBlock = $("<p>")
-//     .addClass("row description col-md-10")
-//     .attr("data-slot", slots[i])
-//     .text(slots[i])
-//     $("#TB").append(timeBlock);
-//     console.log(timeBlock)
-//     for (let i = 8; i < slots.length; i++) {
-//         const element = slots[i];
-//         let timeBlock = $("<textarea>")
-//         .addClass("row description col-md-10")
-//         .attr("data-slot", slots[i])
-//         $("#TB").append(timeBlock);
-//         console.log(timeBlock)
-//         for (let i = 8; i < slots.length; i++) {
-//             const element = slots[i];
-//             let timeBlock = $("<button>")
-//             .addClass("row btn saveBtn col-md-10")
-//             .attr("data-slot", slots[i])
-//             $("#TB").append(timeBlock);
-//             console.log(timeBlock)
-//         }
-//     }
-// }
-
-// for (let i = 0; i < letters.length; i++) {
-//     const element = letters[i];
-//     let letterBtn = $("<button>")
-//       .addClass("letter-button letter letter-button-color")
-//       .attr("data-letter", letters[i])
-//       .text(letters[i]);
-//       $("#buttons").append(letterBtn);
-
-//       console.log(letters);
-//     }
-// ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ commented out attempts ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙
+                    // ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ commented out attempts ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙
+                    // ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ side bar from tucker ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙
+                    //jquery access sibling element - may need for my code... read up on jquery parents. simplying
+                    // $(".saveBtn").("click", function(){
+                      //   let schedule = localStorage.getItem("storage");
+                      //   if(schedule){
+                        //       schedule = JSON.parse(schedule);
+                        //   }else {
+                          //     schedule = {};
+                          //   }
+                          
+                          //   for(id in schedule){
+        //     $("#").find("textarea").val(schedule[id]);
+        //   }
+        
+        //   const val = $(this).siblings("textarea")[0])
+        //   console.log(val);
+        //   const id = $(this).parent().attr("id");
+        //   console.log(id);
+        //   schedule[id] = val;
+        //   localStorage.Storage.setItem("schedule", JSON.stringify(scheulde));
+        // })
+// ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙ side bar from tucker ◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙◙
